df9d59fc988058948c468784bc14245d5f54ec81
johnpauljanecek <johnpauljanecek@gmail.com>
2015-06-03 19:54:28 +0800

    docs

--- rpyc_docker/__init__.py
+++ rpyc_docker/__init__.py
@@ -1,3 +1,3 @@
 from rpyc_docker.manager import Manager
-
-__author__ = "John Paul Janecek (johnpauljanecek@gmail.com)"
+__website__ = "http://johnpauljanecek.github.io/rpyc_docker/"
+__author__  = "John Paul Janecek (johnpauljanecek@gmail.com)"
